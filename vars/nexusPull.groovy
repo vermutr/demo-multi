f4c66eb1bdd5bb7cbd6212368f0df5d9ea59c62d
Roman Vertinsky <raman.viartsinski@azati.com>
2024-03-06 13:28:59 +0100

    Jenkinsfile

--- vars/nexusPull.groovy
+++ vars/nexusPull.groovy
@@ -2,15 +2,15 @@ def call(String imageName, String tag = 'latest', String credentialsId) {
     withCredentials([usernamePassword(credentialsId: credentialsId, passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         sh "echo $PASSWORD | docker login -u $USERNAME --password-stdin 192.168.92.61:8082"
 
-        def dockerPushCommand = "docker pull repository/my-test-repo:latest"
+        def dockerPushCommand = "docker pull 192.168.92.61:8082/repository/my-test-repo:latest"
 
         try {
-            echo "Pushing Docker image to registry: ${imageName}:${tag}"
+            echo "Pulling Docker image to registry: ${imageName}:${tag}"
             sh dockerPushCommand
-            echo "Docker image pushed successfully."
+            echo "Docker image Pulled successfully."
             sh "docker images"
         } catch (Exception e) {
-            echo "Error during Docker push: ${e.message}"
+            echo "Error during Docker Pulling: ${e.message}"
             throw e
         } finally {
             sh "docker logout"
